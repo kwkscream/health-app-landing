5c5ab26079c9a1a9417c6312ebaa853a02f8515f
kwkscream <spoonkicker@gmail.com>
2025-06-11 13:06:27 +0300

    add Montserrat font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
-<script setup></script>
+<script setup>
+import Header from "./components/Header.vue";
+</script>
 
 <template>
-	
+	<Header />
 </template>
 
+<style>
+body {
+	font-family: "Montserrat", sans-serif;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+
+<template>
+	<h1>Hello, im a header!!!</h1>
+</template>
