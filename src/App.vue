4d15dc5039540c89ed5c4de68a824b33a8985e08
kwkscream <spoonkicker@gmail.com>
2025-06-11 18:46:38 +0300

    add tutorial section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Header from "./components/Header.vue";
- import HeroSection from "./components/HeroSection.vue";
+import HeroSection from "./components/HeroSection.vue";
+import Tutorial from "./components/Tutorial.vue"
+
 </script>
 
 <template>
 	<Header />
 	<HeroSection />
+	<Tutorial />
 </template>
 
 <style>
---
+++ src/components/Tutorial.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+	<section class="max-w-[1230px] w-full mx-auto">
+		<div class="flex items-center justify-center">
+			<div class="flex flex-col justify-center items-center">
+				<h2 class="text-5xl font-bold mb-6 text-[#23153E]">
+					How does it work?
+				</h2>
+				<p class="text-4xl font-medium text-[#443A5A] mb-5">
+					Easy, fun & essential training for moms
+				</p>
+				<div class="bg-[#FF2D55] h-[3px] w-[46px] mb-[60px]"></div>
+			</div>
+		</div>
+		<div class="flex gap-10">
+			<div class="flex gap-10 max-w-[596px]">
+				<img
+					src="@/assets/Icons/search_icon.svg"
+					class="w-[76px] h-[76px]"
+				/>
+				<div>
+					<h3 class="text-[#23153E] text-3xl font-medium">
+						Browse exercises
+					</h3>
+					<p class="text-[#443A5A] text-lg">
+						Browse over 100 exercises with a video or picture and a
+						description.
+					</p>
+				</div>
+			</div>
+			<div class="flex gap-10 max-w-[596px]">
+				<img
+					src="@/assets/Icons/create_icon.svg"
+					class="w-[76px] h-[76px]"
+				/>
+				<div>
+					<h3 class="text-[#23153E] text-3xl font-medium">
+						Create timeline
+					</h3>
+					<p class="text-[#443A5A] text-lg">
+						Take pictures along the way during and after the
+						pregnancy to see the transformation.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
