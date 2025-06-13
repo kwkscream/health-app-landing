628abf937664c87d2a4196ced6b2f1e11b926873
kwkscream <spoonkicker@gmail.com>
2025-06-13 12:09:31 +0300

    fix CTA form button

--- src/components/SendDataCTA.vue
+++ src/components/SendDataCTA.vue
@@ -6,10 +6,10 @@
 			<div
 				class="flex flex-col md:flex-row items-center justify-center gap-5 md:gap-[240px] px-[50px] py-[55px] md:px-[100px] md:py-[105px]"
 			>
-				<h3 class="text-white text-2xl md:text-5xl font-bold">Send your data</h3>
+				<h3 class="md:whitespace-nowrap text-white text-2xl md:text-5xl font-bold">Send your data</h3>
 				<RouterLink
 					to="/feedback"
-					class="rounded-full hover:bg-[#6730A4] bg-[#6750A4] py-2 md:py-3.5 px-[50px] md:px-[147px] font-medium text-xl md:text-lg text-white"
+					class="md:whitespace-nowrap rounded-full hover:bg-[#6730A4] bg-[#6750A4] py-2 md:py-3.5 px-[50px] md:px-[147px] font-medium text-xl md:text-lg text-white"
 					>Go to form</RouterLink
 				>
 			</div>
